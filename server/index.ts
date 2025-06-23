71606203dc5815fb914c65b407b97337a810eb2f
Devaryaa <devarya212006@gmail.com>
2025-06-24 02:22:30 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 //import { setupVite, serveStatic, log } from "./vite";
+import { log } from "./log"
 import { seedRewards } from "./seedRewards";
 
 const app = express();
